372f030cff47d2fd930c6eb868cbd76759417819
Mark Chadwick <m.chadwick@gns.cri.nz>
2015-10-22 13:23:57 +1300

    switched to devices and updated file tests

--- location_test.go
+++ location_test.go
@@ -1,86 +1,85 @@
 package metadata
 
 import (
-	"reflect"
+	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/BurntSushi/toml"
-	//	"github.com/ozym/metadata"
 )
 
 func TestLocation_File(t *testing.T) {
+	var err error
 
-	t.Log("Check loading location files.")
+	var l Location
+	t.Log("Check loading location file.")
 	{
+		if _, err = toml.DecodeFile("testdata/location.toml", &l); err != nil {
+			t.Error(err)
+		}
+	}
 
-		n, err := ParseIPNetwork("192.168.192.0/28")
-		if err != nil {
+	var b []byte
+	t.Log("Compare loaded location file.")
+	{
+		if b, err = ioutil.ReadFile("testdata/location.toml"); err != nil {
+			t.Error(err)
+		}
+		if string(b) != l.String() {
+			t.Errorf("location file text mismatch: [\n%s\n]", SimpleDiff(string(b), l.String()))
+		}
+	}
+
+	var f Location
+	t.Log("Compare loaded location details.")
+	{
+		var n *IPNetwork
+		var a [4]*IPAddress
+
+		if n, err = ParseIPNetwork("192.168.192.0/28"); err != nil {
 			t.Error(err)
 			return
 		}
 
-		a1, err := ParseIPAddress("192.168.192.1/28")
-		if err != nil {
-			t.Error(err)
-			return
+		for i := 0; i < 4; i++ {
+			s := fmt.Sprintf("192.168.192.%d/28", i+1)
+			if a[i], err = ParseIPAddress(s); err != nil {
+				t.Error(err)
+				return
+			}
 		}
 
-		a2, err := ParseIPAddress("192.168.192.2/28")
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		a3, err := ParseIPAddress("192.168.192.3/28")
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		a4, err := ParseIPAddress("192.168.192.4/28")
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		f := Location{
+		f = Location{
 			Tag:      "location",
 			Name:     "A Location Name",
 			Latitude: &[]float32{-41.5}[0],
-			//			Longitude: &[]float32{174.5}[0],
-			Runnet: n,
-			Locnet: &[]bool{true}[0],
+			Runnet:   n,
+			Locnet:   &[]bool{true}[0],
 			Linknets: []Linknet{
 				Linknet{Name: "From A to B"},
 				Linknet{},
 				Linknet{Name: "From A to C"},
 			},
-			Equipment: map[string]Located{
-				"test1": Located{
+			Devices: map[string]Device{
+				"test1": Device{
 					Name:        "test1-location",
-					Address:     a1,
-					Aliases:     []IPAddress{*a2, *a3},
+					Address:     a[0],
+					Aliases:     []IPAddress{*a[1], *a[2]},
 					Model:       "Test Model 1",
 					Tags:        []string{"ABCD", "EFG", "HIJ"},
 					Uninstalled: &[]bool{false}[0],
-					Notes:       &[]string{"Some Notes"}[0],
+					Notes:       &[]string{"Some Notes\nSome More Notes"}[0],
 				},
-				"test2": Located{
+				"test2": Device{
 					Name:        "test2-location",
-					Address:     a4,
+					Address:     a[3],
 					Model:       "Test Model 2",
 					Uninstalled: &[]bool{true}[0],
 				},
 			},
 		}
-		var l Location
-		if _, err := toml.DecodeFile("testdata/location.toml", &l); err != nil {
-			t.Error(err)
+		if l.String() != f.String() {
+			t.Errorf("location details mismatch: [\n%s\n]", SimpleDiff(l.String(), f.String()))
 		}
-
-		if !reflect.DeepEqual(l, f) {
-			t.Errorf("location file entry mismatch: %s", "testdata/location.toml")
-		}
-		t.Log(l.String())
 	}
 }
